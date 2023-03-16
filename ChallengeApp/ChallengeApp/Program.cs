03fb10dbee27a933b1a573c4cf8fd1c4367ab90a
LisuGamess <t.kazmierczak@poczta.onet.pl>
2023-03-16 16:15:13 +0100

    Zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie Domowe
+
+string name = "Ewa";
+bool ifWoman = true;
+var age = 33;
+var age2 = 25;
+var age3 = 16;
+
+if (ifWoman = true && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (age2 == 25 && ifWoman == true)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (ifWoman != false && age3 == 16)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
